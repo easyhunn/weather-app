3b802939d3c6c404a3108f8cce0da543d4649a2f
TLE0410 <48020887+TLE0410@users.noreply.github.com>
2020-08-11 03:16:12 +0700

    handle loading weatherEngin and error searching

--- src/Component/WeatherEngin.js
+++ src/Component/WeatherEngin.js
@@ -10,23 +10,35 @@ const WeatherEngin = ({ location }) => {
     temp: "",
     condition: "haze",
   });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({
+    value: false,
+    message: "",
+  });
 
   const getWeather = async (query) => {
+    setLoading(true);
     const apiRes = await fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${query}&appid=11350a089f2935085035c65f58c6d3d5`
     );
     const resJson = apiRes.json();
     resJson.then((resJson) => {
       console.log(resJson.message);
-      if (!resJson.message) {
+      try {
         setWeather({
           country: resJson.sys.country,
           city: resJson.name,
           temp: parseInt((resJson.main.temp - 273.15) * 100) / 100,
           condition: resJson.weather[0].main,
         });
+      } catch (e) {
+        setError({
+          value: true,
+          message: e.message,
+        });
       }
     });
+    setLoading(false);
   };
   const handleSearch = (e) => {
     e.preventDefault();
@@ -39,19 +51,32 @@ const WeatherEngin = ({ location }) => {
 
   return (
     <div>
-      <Component
-        temp={weather.temp}
-        country={{ name: weather.country, city: weather.city }}
-        condition={weather.condition}
-      />
-      <form onSubmit={handleSearch}>
-        <input
-          type="text"
-          onChange={(e) => setQuery(e.target.value)}
-          value={query}
-        />
-        <button>Submit</button>
-      </form>
+      {!loading && !error.value ? (
+        <div>
+          <Component
+            temp={weather.temp}
+            country={{ name: weather.country, city: weather.city }}
+            condition={weather.condition}
+          />
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+            />
+            <button>Submit</button>
+          </form>
+        </div>
+      ) : loading ? (
+        <div style={{ color: "black" }}>On loading</div>
+      ) : !loading && error.value ? (
+        <div>
+          <p style={{ color: "red" }}>{error.message}</p>
+          <button onClick={() => setError({ value: false, message: "" })}>
+            Reset
+          </button>
+        </div>
+      ) : null}
     </div>
   );
 };
